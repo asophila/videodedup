e0f22355f397e44368bc2704b29cb2e4d73bf2ad
bicubico <nunez.alejandro@gmail.com>
2025-03-21 20:50:37 -0300

    Remove cache import from __init__.py
    Removed CacheManager import and export since the cache functionality has been removed.

--- mediadedup/common/__init__.py
+++ mediadedup/common/__init__.py
@@ -3,7 +3,6 @@ Common utilities and shared functionality for media deduplication.
 """
 
 from .models import MediaFile, DuplicateGroup
-from .cache import CacheManager
 from .utils import setup_logging, format_size, find_files, VERSION
 from .actions import handle_duplicates
 from .cli import BaseArgumentParser, VideoArgumentParser, ImageArgumentParser
@@ -11,7 +10,6 @@ from .cli import BaseArgumentParser, VideoArgumentParser, ImageArgumentParser
 __all__ = [
     'MediaFile',
     'DuplicateGroup',
-    'CacheManager',
     'setup_logging',
     'format_size',
     'find_files',
